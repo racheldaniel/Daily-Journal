10ca80ed583ca73f5534c6da436abf7f3f4814fb
Rachel Daniel <rachdaniel88@gmail.com>
2018-10-05 13:47:58 -0500

    Updated script to create an array of journal entries

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,42 @@
 // set choices of moods for later selection
 const moodChoices = document.getElementById("moodChoices");
+const allEntries = [];
+const journal = document.getElementsByClassName("journal")
 
 
 
-// when record journal entry is pressed, store entry values in variables-- console.log to test
+// listen for form submission
 document.addEventListener('submit', (e) => {
+  // prevent page from reloading
   e.preventDefault();
+  // assign input to variables
   let journalDate = document.getElementById("journalDate");
   let concepts = document.getElementById("concepts");
   let journalEntry = document.getElementById("journalEntry");
   let mood = moodChoices.options[moodChoices.selectedIndex];
-  console.log(journalDate.value)
-  console.log(concepts.value)
-  console.log(journalEntry.value)
-  console.log(mood.value)
-  // after logging values, reset form
+
+  //create an object with the input values
+  let currentEntry = {
+    Date: journalDate.value,
+    Concepts: concepts.value,
+    Entry: journalEntry.value,
+    Mood: mood.value,
+  }
+
+  //push object to array of entries
+
+  allEntries.push(currentEntry);
+
+  //  reset form
   journalDate.value = " "
   concepts.value = " "
   journalEntry.value = " "
-  moodChoices.selectedIndex = document.getElementById("moodChoices").firstElementChild 
+  moodChoices.selectedIndex = document.getElementById("moodChoices").firstElementChild
 
-})  
+})
 
+ //loop through array of entries and post to DOM
+
+//  for (let i = 0; i < allEntries.length; i++){
+
+//  }
