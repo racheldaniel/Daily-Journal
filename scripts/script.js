d4f06e1690f9739aedbd7e5d6627f252a072183f
Rachel Daniel <rachdaniel88@gmail.com>
2018-10-05 15:35:11 -0500

    Removed array, now posting entries upon submit

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,57 @@
 // set choices of moods for later selection
 const moodChoices = document.getElementById("moodChoices");
-const allEntries = [];
-const journal = document.getElementsByClassName("journal")
+// const allEntries = [];
+const journal = document.getElementsByClassName("journal")[0]
 
+
+// creates an element, creates text element, appends text to element and adds a class
+function createContent(element, content, className) {
+  elem = document.createElement(element);
+  elementContent = document.createTextNode(content);
+  elem.appendChild(elementContent);
+  elem.className = className;
+  return elem
+}
 
 
 // listen for form submission
-document.addEventListener('submit', (e) => {
+document.addEventListener("submit", (e) => {
   // prevent page from reloading
   e.preventDefault();
   // assign input to variables
   let journalDate = document.getElementById("journalDate");
   let concepts = document.getElementById("concepts");
   let journalEntry = document.getElementById("journalEntry");
-  let mood = moodChoices.options[moodChoices.selectedIndex];
+  let dailyMood = moodChoices.options[moodChoices.selectedIndex];
 
   //create an object with the input values
   let currentEntry = {
-    Date: journalDate.value,
-    Concepts: concepts.value,
-    Entry: journalEntry.value,
-    Mood: mood.value,
+    date: journalDate.value,
+    concepts: concepts.value,
+    entry: journalEntry.value,
+    mood: dailyMood.value,
   }
 
-  //push object to array of entries
+  let date = ("Date: " + currentEntry.date);
+  let conceptsLearned = ("Concepts Covered: " + currentEntry.concepts);
+  let journalEntryText = currentEntry.entry;
+  let mood = ("I'm Feeling: " + currentEntry.mood);
 
-  allEntries.push(currentEntry);
+  let section = document.createElement("section");
+  journal.appendChild(section);
+
+  let dateElement = createContent("h3", date, "date");
+  section.appendChild(dateElement);
+
+  let conceptElement = createContent("p", conceptsLearned, "concepts");
+  section.appendChild(conceptElement);
+
+  let entryElement = createContent("p", journalEntryText, "entry");
+  section.appendChild(entryElement);
+
+  let moodElement = createContent("p", mood, "mood");
+  section.appendChild(moodElement);
+
 
   //  reset form
   journalDate.value = " "
@@ -35,8 +61,11 @@ document.addEventListener('submit', (e) => {
 
 })
 
- //loop through array of entries and post to DOM
 
-//  for (let i = 0; i < allEntries.length; i++){
 
-//  }
+
+
+
+
+
+  
