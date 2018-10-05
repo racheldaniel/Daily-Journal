86dfec928a97f0f2c6c39cc936da893af5eb22d9
Rachel Daniel <rachdaniel88@gmail.com>
2018-10-05 11:27:35 -0500

    input values are cleared after storing

--- scripts/script.js
+++ scripts/script.js
@@ -5,16 +5,18 @@ const moodChoices = document.getElementById("moodChoices");
 // when record journal entry is pressed, store entry values in variables-- console.log to test
 document.addEventListener('submit', (e) => {
   e.preventDefault();
-  let journalDate = document.getElementById("journalDate").value;
-  let concepts = document.getElementById("concepts").value;
-  let journalEntry = document.getElementById("journalEntry").value;
-  let mood = moodChoices.options[moodChoices.selectedIndex].value;
-  console.log(journalDate)
-  console.log(concepts)
-  console.log(journalEntry)
-  console.log(mood)
-  journalDate = " "
-  concepts = " "
+  let journalDate = document.getElementById("journalDate");
+  let concepts = document.getElementById("concepts");
+  let journalEntry = document.getElementById("journalEntry");
+  let mood = moodChoices.options[moodChoices.selectedIndex];
+  console.log(journalDate.value)
+  console.log(concepts.value)
+  console.log(journalEntry.value)
+  console.log(mood.value)
+  journalDate.value = " "
+  concepts.value = " "
+  journalEntry.value = " "
+  // mood.value = "select" This isn't working yet-- console.log runs again as well
 
 })  
 
