6f4504adecf994ea55c4acb7b137ba10038ab8d8
Rachel Daniel <rachdaniel88@gmail.com>
2018-10-05 10:58:08 -0500

    store values of entry in variables wupon submit

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,20 @@
+const moodChoices = document.getElementById("moodChoices");
 
 
 
-// when record journal entry is pressed, console log test
+// when record journal entry is pressed, store entry values in variables-- console.log to test
 document.addEventListener('submit', (e) => {
   e.preventDefault();
-  console.log("Test")
+  let journalDate = document.getElementById("journalDate").value;
+  let concepts = document.getElementById("concepts").value;
+  let journalEntry = document.getElementById("journalEntry").value;
+  let mood = moodChoices.options[moodChoices.selectedIndex].value;
+  console.log(journalDate)
+  console.log(concepts)
+  console.log(journalEntry)
+  console.log(mood)
+  journalDate = " "
+  concepts = " "
 
 })  
+
